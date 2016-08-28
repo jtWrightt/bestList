188226309320cded167671f8beed65623c33e093
JT Wright <wright.jacob25@svvsd.org>
2016-08-27 22:25:09 -0600

    🏡👜 0.0.1

--- README.md
+++ README.md
@@ -8,7 +8,12 @@ tl;dr todo app that goes the extra mile
 
 Change Log
 ------------
-
+v0.0.1
++ Add todo items with text box
++ delete todo items when finished with your task
++ have an idea? add it with the idea text box!
++ vote up or down ideas that you deem worthy
++ highest voted ideas will be added in future updates
 
 Made by JT Wright
 -----------------
--- public/client.js
+++ public/client.js
@@ -72,6 +72,7 @@ $('.idea-list').ready(function() {
   runs showIdeas after
   */
 function pullIdeas() {
+  ideaItems = [];
   firebase.database().ref('ideas/').on('child_added', function(snapshot) {
     ideaItems.push([snapshot.key, snapshot.val().idea, snapshot.val().likes]);
     showIdeas();
@@ -84,14 +85,68 @@ function pullIdeas() {
 function showIdeas() {
   $('.idea-list').empty();
   for(i=0;i<ideaItems.length;i++) {
-    $('.idea-list').append('<li class="'+ideaItems[i][0]+'">' + ideaItems[i][1] + ' <i class="fa fa-thumbs-up" id="'+ideaItems[i][0]+'" aria-hidden="true"></i> <i class="fa fa-thumbs-down" id="'+ideaItems[i][0]+'" aria-hidden="true"></i> Likes: ' +ideaItems[i][2]+ '</li>');
+    $('.idea-list').append('<li class="'+ideaItems[i][0]+'">' + ideaItems[i][1] + ' <i class="fa fa-thumbs-up" id="'+ideaItems[i][0]+'" aria-hidden="true"></i> <i class="fa fa-thumbs-down" id="'+ideaItems[i][0]+'" aria-hidden="true"></i> <span class="likes" id="'+ideaItems[i][0]+'"> Likes: ' +ideaItems[i][2]+ '</span></li>');
   }
 }
 
+/*
+  function for liking an idea
+  does this by adding one to the likes section in hte items firebase db
+  runs updateLikes();
+  runs hideButtons();
+*/
 $('.idea-list').on('click', '.fa-thumbs-up', function() {
-  var likeCount = $('#'+this.id).
-  console.log(this.id);
-  firebase.database().ref('ideas/' + this.id).update({
-    likes
+  var likeCount = 0;
+  firebase.database().ref('ideas/'+this.id+"/likes").on("value", function(snapshot) {
+    likeCount = snapshot.val();
   });
+  firebase.database().ref('ideas/' + this.id).update({
+    likes: likeCount + 1
+  });
+  updateLikes(this.id);
+  hideButtons(this.id);
 });
+
+/*
+  function for disliking an idea
+  subtracts 1 from likes section in firebase db
+  runs updateLikes()
+  runs hideButtons()
+*/
+$('.idea-list').on('click', '.fa-thumbs-down', function() {
+  var likeCount = 0;
+  firebase.database().ref('ideas/'+this.id+"/likes").on("value", function(snapshot) {
+    likeCount = snapshot.val();
+  });
+  firebase.database().ref('ideas/' + this.id).update({
+    likes: likeCount - 1
+  });
+  updateLikes(this.id);
+  hideButtons(this.id);
+});
+
+/*
+  takes in button id, most likly an fa-thumbs-/up/down id
+  function for hiding like/dislike buttons
+  takes both items for the specific id and sets them to not display
+*/
+function hideButtons(id) {
+  $("#"+id + ".fa-thumbs-up").css('display', 'none');
+  $("#"+id + ".fa-thumbs-down").css('display', 'none');
+  $
+}
+
+/*
+  takes in button id, most likly an fa-thumbs-up/down id
+  function for updating a specific idea's like span
+  does this by setting var likeCount to an int of the likes from a firebase db search
+  then sets html of the specific span to the like count and shows new span content
+*/
+function updateLikes(id) {
+  var likeCount;
+  firebase.database().ref('ideas/'+id+"/likes").on("value", function(snapshot) {
+    likeCount = snapshot.val();
+  });
+
+  $('span#'+ id).html(likeCount);   
+}
